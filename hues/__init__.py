d32b2db579538f0455846db79e3a2e9e1a81de82
Adam Watkins <acwatkins@gmail.com>
2015-01-02 16:37:18 -0500

    Added basic functionality for schedules using phue
    Added scheduling based on time and offset from previous time
    Added a way to save off settings that are commonly used and refer to them by name
    Added a setup script to be able to install

---
+++ hues/__init__.py
@@ -0,0 +1,125 @@
+#!/usr/bin/env python3
+
+import datetime
+import pytz
+import time
+
+from phue import Bridge
+
+class LightProperties(object):
+	brightness = None
+	colorTemperature = None
+	hue = None
+	saturation = None
+
+	def __init__(self):
+		super(LightProperties, self).__init__()
+
+	def getConfig(self):
+		config = {}
+		config['bri'] = self.brightness
+		if (self.hue != None and self.saturation != None):
+			config['hue'] = self.hue
+			config['sat'] = self.saturation
+
+		if (self.colorTemperature != None):
+			config['ct'] = self.colorTemperature
+
+		return config
+
+
+class Schedule(object):
+	lightProperties = {}
+	localtz = pytz.timezone ("GMT")
+	bridge = Bridge('huebridge', 'newdeveloper')
+	lastEventTimeUsed = None
+
+	def __init__(self):
+		super(Schedule, self).__init__()
+		self.bridge.connect()
+
+		energize = LightProperties()
+		energize.brightness = 237
+		energize.colorTemperature = 155
+
+		reading = LightProperties()
+		reading.brightness = 240
+		reading.colorTemperature = 343
+
+		concentrate = LightProperties()
+		concentrate.brightness = 219
+		concentrate.colorTemperature = 234
+
+		relax = LightProperties()
+		relax.brightness = 144
+		relax.colorTemperature = 467
+
+		yellowSun = LightProperties()
+		yellowSun.brightness = 150
+		yellowSun.hue = 7826
+		yellowSun.saturation = 250
+
+		white = LightProperties()
+		white.brightness = 220
+		white.hue = 0
+		white.saturation = 0
+
+		orangeLow = LightProperties()
+		orangeLow.brightness = 1
+		orangeLow.hue = 2912
+		orangeLow.saturation = 254
+
+		self.lightProperties['energize'] = energize
+		self.lightProperties['reading'] = reading
+		self.lightProperties['concentrate'] = concentrate
+		self.lightProperties['relax'] = relax
+		self.lightProperties['yellowSun'] = yellowSun
+		self.lightProperties['orangeLow'] = orangeLow
+		self.lightProperties['white'] = white
+		
+	def setTimeZone(self, timezone):
+		self.localtz = pytz.timezone(timezone)
+
+	def registerLightSetting(self, name, setting):
+		self.lightProperties[name] = setting
+
+	def addEvent(self, hour, minute, second, name, settingName, transitionTimeInSeconds, lightOn = None):
+		self.addGroupEvent(hour, minute, second, [ name ], settingName, transitionTimeInSeconds, lightOn)
+
+	def addGroupEvent(self, hour, minute, second, names, settingName, transitionTimeInSeconds, lightOn = None):
+		localTime = self.getLocalDateTime(hour, minute, second)
+		config = self.getLightConfiguration(settingName, transitionTimeInSeconds, lightOn)
+		print(str(config))
+		for name in names:
+			self.bridge.create_schedule(settingName, self.getUtcTimeString(localTime), self.bridge.get_light_id_by_name(name), config, settingName)
+		self.lastEventTimeUsed = localTime
+
+	def addEventByOffsetToLast(self, secondsSinceLastEvent, name, settingName, transitionTimeInSeconds, lightOn = None):
+		self.addGroupEventByOffsetToLast(secondsSinceLastEvent, [name], settingName, transitionTimeInSeconds, lightOn)
+
+	def addGroupEventByOffsetToLast(self, secondsSinceLastEvent, names, settingName, transitionTimeInSeconds, lightOn = None):
+		if (self.lastEventTimeUsed != None):
+			self.lastEventTimeUsed += datetime.timedelta(seconds = secondsSinceLastEvent)
+			self.addGroupEvent(self.lastEventTimeUsed.hour, self.lastEventTimeUsed.minute, self.lastEventTimeUsed.second, names, settingName, transitionTimeInSeconds, lightOn)
+		else:
+			print("Error, called addEventByOffsetToLast without adding an initial event")
+
+	def getLightConfiguration(self, settingName, transitionTimeInSeconds, lightOn):
+		lightProperties = self.lightProperties[settingName]
+		config = lightProperties.getConfig()
+		if (lightOn != None):
+			config['on'] = lightOn
+
+		config['transitiontime'] = transitionTimeInSeconds * 10
+
+		return config
+
+	def getUtcTimeString(self, dateTime):
+		utcDateTime = dateTime.astimezone(pytz.utc)
+		return utcDateTime.strftime("%Y-%m-%dT%H:%M:%S")
+
+	def getLocalDateTime(self, hour, minute, second):
+		localTime = datetime.datetime.now()
+		localTime = localTime.replace(hour = hour, minute = minute, second = second)
+		localTime = self.localtz.localize(localTime)#, is_dst=None)
+		return localTime
---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='hues',
+      version='0.1',
+      description='A Philips hue scheduler',
+      url='https://github.com/acwatkins/hues',
+      author='Adam Watkins',
+      author_email='acwatkins@gmail.com',
+      license='GPL3',
+      packages=['hues'],
+      zip_safe=False)
